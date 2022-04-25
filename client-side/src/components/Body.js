9dffc96f643fce64f47ee8824ae8e56d8ba60143
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-25 14:17:33 +0100

    added tailwind, connected to metamask

---
+++ client-side/src/components/Body.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.png";
+import { ethers }from "ethers";
+import { use } from "chai";
+
+function Body() {
+    const [greet, setGreet] = useState('');
+    const [depositValue, setDepositValue] = useState('');
+    const [greetingValue, setGreetingValue] = useState('');
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner()
+    
+    useEffect(() => {
+        const connectWallet = async () => {
+            await provider.send("eth_requestAccounts", []);
+
+            const getBalance = async () => {
+             
+            }
+        }
+        connectWallet()
+        .catch(console.error);
+    })
+
+    const handleDepositChange = (e) => {
+        setDepositValue(e.target.value);
+    }
+
+    const handleGreetingChange = (e) => {
+        setGreetingValue(e.target.value);
+    }
+
+    const handleDepositSubmit = (e) => {
+        e.preventDefault();
+        console.log(depositValue)
+    }
+
+    const handleGreetingSubmit = (e) => {
+        e.preventDefault();
+        console.log(greetingValue)
+    }
+
+  return (
+    <main>
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <h2 className="text-center">Hello, There</h2>
+        <p className="text-center">Your Contract-Balance</p>
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8">
+            <div>
+              <img className="mx-auto h-12 w-auto" src={logo} />
+            </div>
+            <form className="mt-8 space-y-6" 
+            action="#" 
+            method="POST"
+            onSubmit={handleGreetingSubmit}
+            >
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div></div>
+                <div>
+                  <label htmlFor="Change" className="sr-only">
+                    Change
+                  </label>
+                  <input
+                    id="Change"
+                    name="Change"
+                    type="Change"
+                    onChange={handleDepositChange} value={depositValue}
+                    required
+                    className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-b-md
+                  focus:outline-none focus:ring-gray-900
+                  focus:border-gray-900 focus:z-10 sm:text-sm"
+                    placeholder="Change"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center
+                py-2 px-4 border border-transparent text-sm font-medium
+                rounded-md text-white bg-gray-900 hover:bg-gray-900
+                focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-gray-900"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+                  Change
+                </button>
+              </div>
+            </form>
+            <form className="mt-8 space-y-6" 
+            action="#" 
+            onSubmit={handleDepositSubmit}
+            method="POST">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div></div>
+                <div>
+                  <label htmlFor="Deposit" className="sr-only">
+                    Deposit
+                  </label>
+                  <input
+                    id="number"
+                    name="number"
+                    type="number"
+                    onChange={handleGreetingChange}
+                    value={greetingValue}
+                    required
+                    className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-b-md
+                  focus:outline-none focus:ring-gray-900
+                  focus:border-gray-900 focus:z-10 sm:text-sm"
+                    placeholder="Deposit"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center
+                py-2 px-4 border border-transparent text-sm font-medium
+                rounded-md text-white bg-gray-900 hover:bg-gray-900
+                focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-gray-900"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+                  Deposit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Body;
---
+++ client-side/src/components/Navigation.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import logo from "./logo.png";
+
+function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="bg-slate-900">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img className="h-8 w-8" src={logo} alt="Workflow" />
+              </div>
+              <div className="hidden md:block"></div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
